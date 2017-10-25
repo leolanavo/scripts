375b3411cc4438fb525ad49b5d758da093c67b27
Leonardo Lana <leolanavo@gmail.com>
2017-10-25 15:59:50 -0200

    Upgrade the ArchLinux installation script

--- arch_install.sh
+++ arch_install.sh
@@ -1,28 +1,89 @@
 #!/bin/bash
 
-#Network Packages
+# Boot manager packages
+pacman -S refind-efi os-prober
+refind-install --usedefault $1
+mkrlconf
+nano /boot/refind_linux.conf
+
+# Setting up locale
+nano /etc/locale.gen
+locale-gen
+echo LANG=en_US.UTF-8 > /etc/locale.conf
+
+# Setting up NTP and Time
+ls -s /usr/share/zoneinfo/Brazil/East > /etc/localtime
+hwclock --systohc --utc
+pacman -S ntp --noconfirm
+timedatectl set-ntp true
+timedatectl set-timezone America/Sao_Paulo
+systemctl enable ntpd.service
+
+# Hostname setting
+echo $3 > /etc/hostname
+
+# Expanding MirroList
+echo "[multilib]" >> /etc/pacman.conf
+echo "Include = /etc/pacman.d/mirrorlist" >> /etc/pacman.conf
+echo "[archlinuxfr]" >> /etc/pacman.conf
+echo "SigLevel = Never" >> /etc/pacman.conf
+echo "Server = http://repo.archlinux.fr/$arch" >> /etc/pacman.conf
+pacman -Sy
+
+# Add user
+useradd -m -g users -G wheel,storage,power -s /bin/bash $2
+
+# Add sudo power to new user
+echo "%wheel ALL=(ALL) ALL"
+
+# Set up passwords
+echo "Set up the root password"
+passwd
+echo "Set up your own password"
+passwd $2
+
+# Xorg packages
+pacman -S xorg-server xorg-server-utils xorg-xinit \
+          xorg-twm xorg-xclock xterm xclip xorg-xrandr
+
+# Network packages
 pacman -S wpa_supplicant dialog networkmanager openssh networkmanager-applet
 systemctl enable NetworkManager.service
 
-#Xorg Packages
-pacman -S xorg-xinput xf86-input-synaptics
+# Driver packages
+pacman -S nvidia mesa mesa-libgl lib32-mesa-libgl \
+          acpi intel-ucode libinput pulseaudio alsa-utils \
+          bluez bluez-utils --noconfirm
 
-#Video Driver and GUI Packages
-pacman -S nvidia mesa-libgl gnome gdm gnome-extra gnome-tweak-tool
-systemctl enable gdm.service
+# GNOME packages
+pacman -S gnome gdm gnome-extra gnome-tweak-tool --noconfirm
 
-#Boot Manager Packages
-pacman -S refind-efi efibootmgr 
-mkrlconf
-nano /boot/refind_
+# LightDM packages
+pacman -S lightdm --noconfirm
+systemctl enable lightdm.service
 
-#Apps Packages
-pacman -S file-roller vim tmux htop sublime-text texstudio atril jre7-openjdk pacman -S jre7-openjdk python3
+# Apps packages
+pacman -S file-roller neovim tmux htop texstudio evince ranger \
+          termite compton zsh feh maim sxiv mpd pavucontrol \
+          blueberry wget cmatrix atril emacs gimp inkscape \
+          rofi vlc yaourt --noconfirm
 
-#AUR Packages
-yaourt -S google-chrome spotify gnome-software
+# Tex packages
+pacman -S texlive-core texlive-lang --noconfirm
 
-#Vundle and Powerline installations
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-git clone https://github.com/powerline/fonts.git
-./fonts/install.sh
+# Language packages
+pacman -S python3 ruby jre9-openjdk-headless jre9-openjdk \
+          jdk9-openjdk openjdk9-doc openjdk9-src nodejs npm \
+          racket --noconfirm
+
+# AUR packages
+yaourt -S google-chrome spotify-client gnome-software \
+          neovim-symlinks polybar-git i3-gaps-git \
+          lightdm-webkit2-greeter gnome-software \
+          sublime-text-dev neofetch light discord \
+          lightdm-webkit2-theme-material2 --noconfirm
+
+# Dotfiles installation
+git clone http://github.com/leolanavo/dotfiles /home/lana/dotfiles
+cd /home/lana/dotfiles
+./install all
